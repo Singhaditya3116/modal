97abda587f523520fa135e2586931b96eb4cf41a
Singhaditya3116 <singhaditya3116@gmail.com>
2024-12-19 10:34:54 +0530

    fix: disableed useEffect dependency

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect,useRef } from "react";
 
 
@@ -17,6 +18,7 @@ const Modal = ({ modalTitle, onClose, children }) => {
       modalEle.removeEventListener("click", handleOverlayClick);
     };
     
+
   },[])
 
   return (
